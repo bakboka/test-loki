eca85f06d414d7c53d073f68dcefaf883e56c67b
bakboka <baa@skysize.io>
2025-10-05 16:46:58 +0200

    test 1

---
+++ myaddons/mymodule/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Skysize Theme",
+    "summary": """Skysize Theme""",
+    "category": "",
+    "version": "18.0.1.0.0",
+    "author": "skysize.io",
+    "website": "https://www.skysize.io",
+    "license": "LGPL-3",
+    }
